3c8c148d655b816003c2e7d3b9f814fe24346335
joseDev <josevelasquezguerrerroo@gmail.com>
2025-10-19 17:26:15 -0300

    .

--- database.py
+++ database.py
@@ -1,19 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+# Variables del Pooler (no del connection string directo)
+USER = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASSWORD")
+HOST = os.getenv("DB_HOST")  # Este debería terminar en `pooler.supabase.com`
+PORT = os.getenv("DB_PORT", 6543)  # 6543 es el puerto del transaction pooler
+DBNAME = os.getenv("DB_NAME")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
-engine = create_engine(
-    DATABASE_URL,
-    echo=True,  
-)
+# 🚨 Importante: usar NullPool con transaction pooler
+engine = create_engine(DATABASE_URL, poolclass=NullPool)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
